d279e3334cf0fa67f8888961d0f246d6fdcc7e4e
Levi Dantas <levibdantas@gmail.com>
2025-01-21 02:27:05 -0300

    build: Connect h2 to the application

--- src/main/java/solutions/dantas/CadastroDeNinjas/Missoes/MissoesRepository.java
+++ src/main/java/solutions/dantas/CadastroDeNinjas/Missoes/MissoesRepository.java
@@ -4,3 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {
 }
+
+
--- src/main/java/solutions/dantas/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/solutions/dantas/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -20,10 +20,14 @@ public class NinjaModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "nome")
     private String nome;
+
+
     @Column(unique = true)
     private String email;
 
+    @Column(name = "idade")
     private int idade;
 
 
