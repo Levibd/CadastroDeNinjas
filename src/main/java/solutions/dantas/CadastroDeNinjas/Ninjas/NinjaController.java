ec060ce4be5409207bf421b0016da9aa0cefac7e
Levi Dantas <levibdantas@gmail.com>
2025-01-23 02:36:20 -0300

    feat: Updated controller with datas mock

--- src/main/java/solutions/dantas/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/solutions/dantas/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package solutions.dantas.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,5 +9,38 @@ public class NinjaController {
     @GetMapping("/boasVindas")
     public String boasVindas(){
         return "Essa é minha mensagem nessa rota";
+    }
+
+    // Adicionar Ninja
+    @PostMapping("/create")
+    public String criarNinja(){
+        return "Ninja created!";
+    }
+
+
+    // Procurar Ninja por ID
+    @GetMapping("/allId")
+    public String mostrarNinjasId(){
+        return "Ninja by ID";
+    }
+
+
+    // Mostrar todos os ninjas
+    @GetMapping("/all")
+    public String mostrarNinjas(){
+        return "Todos os ninjas";
+    }
+
+    // Alterar dados dos ninjas
+    @PutMapping("/alter")
+    public String alterarNinjaId(){
+        return "Ninja altered!";
+    }
+
+
+    // Deletar Ninja
+    @DeleteMapping("/delete")
+    public String deleteId(){
+        return "Ninja deleted!";
     }
 }
